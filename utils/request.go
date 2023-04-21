8290b2f07c9154ae67acc2e2d89bf995f5bca386
Kholiq <kholiqcode@gmail.com>
2023-04-21 13:11:36 +0700

    feat: create utils request

---
+++ utils/request.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func EnableCORS(r *chi.Mux) {
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*", "wwww.*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+}
