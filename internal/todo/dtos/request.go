af3c67ec422baeb7a34b24418bba4c80cd0d4740
Kholiq <kholiqcode@gmail.com>
2023-04-22 18:43:22 +0700

    feat: create get todos request

--- internal/todo/dtos/request.go
+++ internal/todo/dtos/request.go
@@ -2,13 +2,17 @@ package dtos
 
 type CreateTodoRequest struct {
 	Title           string `json:"title" validate:"required"`
-	ActivityGroupID int    `json:"activity_group_id" validate:"required,gte=1,int"`
+	ActivityGroupID int32  `json:"activity_group_id" validate:"required,gte=1,int"`
 	IsActive        bool   `json:"is_active" validate:"required,oneof=true false"`
 }
 
 type UpdateTodoRequest struct {
 	Title           string `json:"title" validate:"required"`
-	ActivityGroupID int    `json:"activity_group_id" validate:"required,gte=1,int"`
+	ActivityGroupID int32  `json:"activity_group_id" validate:"required,gte=1,int"`
 	IsActive        bool   `json:"is_active" validate:"required,oneof=true false"`
 	Priority        string `json:"priority" validate:"required,oneof=low medium high very-high very-low"`
+}
+
+type GetTodosRequest struct {
+	ActivityGroupID int32 `json:"activity_group_id" validate:"required,gte=1,int"`
 }
