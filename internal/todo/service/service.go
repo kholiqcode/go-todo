e79baf0447e08f8423156e55896eda6729a1668a
Kholiq <kholiqcode@gmail.com>
2023-04-22 19:24:50 +0700

    feat: create store todo service

--- internal/todo/service/service.go
+++ internal/todo/service/service.go
@@ -11,7 +11,7 @@ import (
 type TodoService interface {
 	FindAll(ctx context.Context, request dtos.GetTodosRequest) ([]dtos.TodoResponse, error)
 	FindByID(ctx context.Context, id int32) (*dtos.TodoResponse, error)
-	// Store(ctx context.Context, request dtos.CreateTodoRequest) (*dtos.TodoResponse, error)
+	Store(ctx context.Context, request dtos.CreateTodoRequest) (*dtos.TodoResponse, error)
 	// Update(ctx context.Context, id int32, request dtos.UpdateTodoRequest) (*dtos.TodoResponse, error)
 	// Delete(ctx context.Context, id int32) error
 }
@@ -50,6 +50,35 @@ func (s *todoServiceImpl) FindByID(ctx context.Context, id int32) (*dtos.TodoRes
 
 	if err != nil {
 		return nil, utils.CustomError("failed to get todo", 400)
+	}
+
+	todoResp := dtos.ToTodoResponse(todo)
+
+	return &todoResp, nil
+}
+
+func (s *todoServiceImpl) Store(ctx context.Context, request dtos.CreateTodoRequest) (*dtos.TodoResponse, error) {
+	param := querier.CreateTodoParams{
+		Title:           request.Title,
+		ActivityGroupID: int32(request.ActivityGroupID),
+		IsActive:        request.IsActive,
+		Priority:        "very-high",
+	}
+
+	res, err := s.repo.CreateTodo(ctx, param)
+
+	if err != nil {
+		return nil, utils.CustomErrorWithTrace(err, "failed to create todo", 400)
+	}
+
+	insertedID, err := res.LastInsertId()
+	if err != nil {
+		return nil, utils.CustomErrorWithTrace(err, "failed to get last inserted id", 400)
+	}
+
+	todo, err := s.repo.GetTodo(ctx, int32(insertedID))
+	if err != nil {
+		return nil, utils.CustomErrorWithTrace(err, "failed to get todo", 400)
 	}
 
 	todoResp := dtos.ToTodoResponse(todo)
