f19e4081738433b7cff4a0bead411f82afa56130
Kholiq <kholiqcode@gmail.com>
2023-04-22 18:45:06 +0700

    feat: define todo sql queries

--- internal/todo/queries/todos.sql
+++ internal/todo/queries/todos.sql
@@ -3,7 +3,22 @@ SELECT * FROM todos
 WHERE id = ? LIMIT 1;
 
 -- name: ListTodos :many
-SELECT * FROM todos ORDER BY id DESC;
+SELECT * FROM todos WHERE 
+CASE 
+WHEN sqlc.arg(search_field) = 'activity_group_id' THEN activity_group_id 
+ELSE title END =
+CASE
+WHEN sqlc.arg(search_field) = '' THEN ''
+ELSE sqlc.arg(search_value) END
+ORDER BY 
+CASE WHEN sqlc.arg(sort_by) = 'title' THEN title END ASC,
+CASE WHEN sqlc.arg(sort_by) = '-title' THEN title END DESC,
+CASE WHEN sqlc.arg(sort_by) = 'is_active' THEN is_active END ASC,
+CASE WHEN sqlc.arg(sort_by) = '-is_active' THEN is_active END DESC,
+CASE WHEN sqlc.arg(sort_by) = 'createdAt' THEN created_at END ASC,
+CASE WHEN sqlc.arg(sort_by) = '-createdAt' THEN created_at END DESC, 
+CASE WHEN sqlc.arg(sort_by) = '' THEN activity_group_id END ASC
+LIMIT ? OFFSET ?;
 
 -- name: DeleteTodo :exec
 DELETE FROM todos
