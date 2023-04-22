5c02ca6bfa1b59d1523e441ebfaa348aeefde9fb
Kholiq <kholiqcode@gmail.com>
2023-04-22 18:45:44 +0700

    feat: get all todos

--- internal/todo/service/service.go
+++ internal/todo/service/service.go
@@ -9,19 +9,27 @@ import (
 )
 
 type TodoService interface {
-	FindAll(ctx context.Context) ([]dtos.TodoResponse, error)
-	FindByID(ctx context.Context, id int32) (*dtos.TodoResponse, error)
-	Store(ctx context.Context, request dtos.CreateTodoRequest) (*dtos.TodoResponse, error)
-	Update(ctx context.Context, id int32, request dtos.UpdateTodoRequest) (*dtos.TodoResponse, error)
-	Delete(ctx context.Context, id int32) error
+	FindAll(ctx context.Context, request dtos.GetTodosRequest) ([]dtos.TodoResponse, error)
+	// FindByID(ctx context.Context, id int32) (*dtos.TodoResponse, error)
+	// Store(ctx context.Context, request dtos.CreateTodoRequest) (*dtos.TodoResponse, error)
+	// Update(ctx context.Context, id int32, request dtos.UpdateTodoRequest) (*dtos.TodoResponse, error)
+	// Delete(ctx context.Context, id int32) error
 }
 
 type todoServiceImpl struct {
 	repo querier.TodoRepo
 }
 
-func (s *todoServiceImpl) FindAll(ctx context.Context) ([]dtos.TodoResponse, error) {
-	todos, err := s.repo.ListTodos(ctx)
+func (s *todoServiceImpl) FindAll(ctx context.Context, request dtos.GetTodosRequest) ([]dtos.TodoResponse, error) {
+
+	params := querier.ListTodosParams{
+		SearchField: "activity_group_id",
+		SearchValue: request.ActivityGroupID,
+		Limit:       100,
+		Offset:      0,
+	}
+
+	todos, err := s.repo.ListTodos(ctx, params)
 
 	if err != nil {
 		return nil, utils.CustomError("failed to get todos", 400)
@@ -36,4 +44,3 @@ func (s *todoServiceImpl) FindAll(ctx context.Context) ([]dtos.TodoResponse, err
 
 	return todosResp, nil
 }
-
