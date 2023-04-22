4e5dce539d3ebf1a4516c5981c1b0c045d449236
Kholiq <kholiqcode@gmail.com>
2023-04-22 19:31:03 +0700

    refactor: update todo dto

--- internal/todo/dtos/request.go
+++ internal/todo/dtos/request.go
@@ -7,10 +7,7 @@ type CreateTodoRequest struct {
 }
 
 type UpdateTodoRequest struct {
-	Title           string `json:"title" validate:"required"`
-	ActivityGroupID int    `json:"activity_group_id" validate:"required,gte=1,int"`
-	IsActive        bool   `json:"is_active" validate:"oneof=true false" default:"true"`
-	Priority        string `json:"priority" validate:"oneof=low medium high very-high very-low" default:"low"`
+	Title string `json:"title" validate:"required"`
 }
 
 type GetTodosRequest struct {
