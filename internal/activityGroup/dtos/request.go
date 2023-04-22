ed1211a129ef7b6d5fa7e03b94fdcab2b9825f71
Kholiq <kholiqcode@gmail.com>
2023-04-22 14:23:53 +0700

    feat: create patch activity group dto

--- internal/activityGroup/dtos/request.go
+++ internal/activityGroup/dtos/request.go
@@ -4,3 +4,7 @@ type CreateActivityGroupRequest struct {
 	Title string `json:"title" validate:"required,min=3"`
 	Email string `json:"email" validate:"required,email"`
 }
+
+type UpdateActivityGroupRequest struct {
+	Title string `json:"title" validate:"required,min=3"`
+}
