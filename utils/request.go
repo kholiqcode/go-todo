71ca0b9df5ec15b6bb9742a73d045926619094ae
Kholiq <kholiqcode@gmail.com>
2023-04-22 18:44:07 +0700

    feat: add validate url param request

--- utils/request.go
+++ utils/request.go
@@ -1,9 +1,44 @@
 package utils
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/google/uuid"
 )
+
+type ParamType interface {
+	uuid.UUID | string
+}
+
+func ValidateUrlParamUUID(r *http.Request, paramName string) uuid.UUID {
+	param := chi.URLParam(r, paramName)
+
+	uuid, err := uuid.Parse(param)
+	if err != nil {
+		PanicIfError(CustomErrorWithTrace(err, fmt.Sprintf("invalid %s param", paramName), 400))
+	}
+
+	return uuid
+}
+
+func ValidateQueryParamInt(r *http.Request, queryName string) int {
+	query := r.URL.Query().Get(queryName)
+
+	queryInt, err := strconv.Atoi(query)
+	if err != nil {
+		PanicIfError(CustomErrorWithTrace(err, fmt.Sprintf("invalid %s query", queryName), 400))
+	}
+
+	if queryInt < 0 {
+		PanicIfError(CustomError(fmt.Sprintf("invalid %s query", queryName), 400))
+	}
+
+	return queryInt
+}
 
 func EnableCORS(r *chi.Mux) {
 	r.Use(cors.Handler(cors.Options{
